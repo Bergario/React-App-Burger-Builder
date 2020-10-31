97c7aa33d9bd0e563f28c9e3b86751259f5605d7
bergario <bemby.ber@gmail.com>
2020-11-01 01:17:58 +0700

    Create Layout component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <div></div>;
+import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
