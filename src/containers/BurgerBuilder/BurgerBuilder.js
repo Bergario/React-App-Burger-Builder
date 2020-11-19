d368b2ab9108faadc79ad2abe87473d1357be0b8
bergario <bemby.ber@gmail.com>
2020-11-19 16:15:29 +0700

    creating the Firebase Project, Axios and sending a POST request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENTS_PRICE = {
   Salad: 0.6,
@@ -84,7 +85,24 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("Tareekk Sesss!");
+    const order = {
+      ingredients: this.state.ingredients,
+      totalPrice: this.state.totalPrice,
+      customer: {
+        name: "Bemby",
+        address: {
+          street: "Yogyakarta",
+          zipCode: "59155",
+          country: "Indonesia",
+        },
+        email: "asudah@test.com",
+      },
+      deliveryMethod: "fastest",
+    };
+    axios
+      .post("/orders.json", order)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   };
 
   render() {
