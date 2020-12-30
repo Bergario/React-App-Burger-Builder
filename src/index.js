4d8d8804094a8dd8ac36ac5b70f7e3436913ddbe
bergario <bemby.ber@gmail.com>
2020-12-31 01:26:16 +0700

    npm starnMerge branch 'main' of https://github.com/Bergario/React-App-Burger-Builder into main

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import thunk from "redux-thunk";
 
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import reducer from "./store/reducer";
 
-const store = createStore(reducer);
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log("ACTION", action);
+      const result = next(action);
+      console.log("RESULT", result);
+      return result;
+    };
+  };
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(logger, thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
