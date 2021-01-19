bd5632f4f23bf3941096c5462248cde41f0a2637
bergario <bemby.ber@gmail.com>
2021-01-19 16:58:21 +0700

    Testing

--- src/hoc/asyncComponent/asyncComponent.js
+++ src/hoc/asyncComponent/asyncComponent.js
@@ -8,13 +8,10 @@ export const asyncComponent = (importComponent) => {
 
     componentDidMount() {
       importComponent().then((cmp) => {
-        console.log(cmp.default);
         this.setState({ component: cmp.default });
       });
     }
     render() {
-      console.log(this.props);
-
       const C = this.state.component;
       return C ? <C {...this.props} /> : null;
     }
